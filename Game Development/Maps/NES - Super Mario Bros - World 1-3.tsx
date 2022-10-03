d4ab3d9af13564ebcfaf5f625d8b240b7567684e
Jeremy Wood <112590366+woodjer18@users.noreply.github.com>
2022-10-03 17:39:00 -0400

    Delete Game Development directory

--- Game Development/Maps/NES - Super Mario Bros - World 1-3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="NES - Super Mario Bros - World 1-3" tilewidth="16" tileheight="16" tilecount="2460" columns="164">
- <image source="../../../Downloads/NES - Super Mario Bros - World 1-3.png" width="2624" height="240"/>
-</tileset>
