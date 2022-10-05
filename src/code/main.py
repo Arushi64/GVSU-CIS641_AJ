c941b9e2f56958fe9e5d50727f482d93b58b4fab
Arushi <arushi.k.sharma@gmail.com>
2022-10-05 19:42:38 -0400

    update main

--- src/code/main.py
+++ src/code/main.py
@@ -2,10 +2,13 @@ import pygame
 import sys
 from settings import *
 from level import Level
+# from support import import_csv_layout
 from game_data import level_0
+# from tiles import *
 
 # not using specs below- instead using specs declared in settings.py
 # width, height = 1280, 720
+pygame.init()
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Nostalgia")
 clock = pygame.time.Clock()
@@ -14,32 +17,41 @@ level = Level(level_0, window)
 
 white = (255,255,255)
 color1 = (250,235,215)
-
 FPS = 60
 
-def draw_window():
-    window.fill(color1)
-    pygame.display.update()
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	
+	window.fill(color1)
+	level.run()
 
+	pygame.display.update()
+	clock.tick(FPS)
 
+# def draw_window():
+#     window.fill(color1)
+#     pygame.display.update()
 
-def main(): 
-    run = True
+# def main(): 
+#     run = True
 
-    # game will run until run is not true or you exit out of it
-    while run:
+#     # game will run until run is not true or you exit out of it
+#     while run:
 
-        # check for events in pygame
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                sys.exit()
+#         # check for events in pygame
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 # sys.exit()
         
-        level.run()
-        draw_window()
+#         level.run()
+#         draw_window()
         
 
-    pygame.quit()
+#     pygame.quit()
 
 
 
