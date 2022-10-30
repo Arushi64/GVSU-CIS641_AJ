2754d60e68b146e8d05413d9997c72c05511d335
Arushi <arushi.k.sharma@gmail.com>
2022-10-30 19:16:34 -0400

    update tiled map path

--- src/code/main.py
+++ src/code/main.py
@@ -45,7 +45,7 @@ while True:
 			sys.exit()
 	
 	window.fill(color1)
-	blit_all_tiles(window = pygame.display.set_mode((1280, 720)),tmxdata = load_pygame("src/code/0/level_0.tmx"),world_offset =[0,0])
+	blit_all_tiles(window = pygame.display.set_mode((1280, 720)),tmxdata = load_pygame("C:/Users/arush/Downloads/2D-Mario-style-platformer-main/2 - Level/2 - Level/levels/level_data/level_0.tmx"),world_offset =[0,0])
 
 	level.run()
 
