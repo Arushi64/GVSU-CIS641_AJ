038d669635d06c1227e11941daed4a0fe296b2aa
Arushi <arushi.k.sharma@gmail.com>
2022-10-30 21:01:59 -0400

    added new tiled map, updated directories in tsf files and added png files to local directory

---
+++ src/code/1/Foreground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Foreground" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="C:/Users/arush/OneDrive/Documents/GVSU/Fall 22/641/Github 641/GVSU-CIS641_AJ/src/code/1/Tileset [Levels]/Foreground/Foreground.png" width="128" height="128"/>
+</tileset>
