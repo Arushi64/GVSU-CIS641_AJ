05ac2af660acfeaa1d64b87845fc9750684f206f
Jeremy Wood <112590366+woodjer18@users.noreply.github.com>
2022-11-29 17:40:21 -0500

    4 levels

--- src/code/levels/game_data.py
+++ src/code/levels/game_data.py
@@ -56,25 +56,12 @@ level_4 = {
     'coins': 'src/code/levels/0/0_coins.csv',
     'bg palms': 'src/code/levels/0/0_bg palms.csv',
     'node_pos': (880,210),
-    'unlock': 5,
+    'unlock': 4,
     'node_graphics': 'src/code/levels/graphics/overworld/4'}
-level_5 = {
-    'terrain': 'src/code/levels/0/0_terrain.csv',
-    'player': 'src/code/levels/0/0_player.csv',
-    'fg palms': 'src/code/levels/0/0_fg palms.csv',
-    'enemies': 'src/code/levels/0/0_enemies.csv',
-    'crates': 'src/code/levels/0/0_crates.csv',
-    'constraints': 'src/code/levels/0/0_constraints.csv',
-    'coins': 'src/code/levels/0/0_coins.csv',
-    'bg palms': 'src/code/levels/0/0_bg palms.csv',
-    'node_pos': (1050,400),
-    'unlock': 5,
-    'node_graphics': 'src/code/levels/graphics/overworld/5'}
 
 levels = {
     0: level_0,
     1: level_1,
     2: level_2,
     3: level_3,
-    4: level_4,
-    5: level_5}
+    4: level_4}
