0e81081fe60f944341a30f190bd76d9b9ac07a54
Jeremy Wood <112590366+woodjer18@users.noreply.github.com>
2022-12-08 21:38:32 -0500

    update

--- src/code/levels/main.py
+++ src/code/levels/main.py
@@ -14,35 +14,22 @@ class Game:
         self.cur_health = 100
         self.coins = 0
 
-        #audio 
-        self.level_bg_music = pygame.mixer.Sound(r'src/code/levels/audio/level_music.wav')
-        self.level_bg_music.set_volume(0.05)
-        self.overworld_bg_music = pygame.mixer.Sound(r'src/code/levels/audio/overworld_music.wav')
-        self.overworld_bg_music.set_volume(0.05)
-
         # overworld creation
         self.overworld = Overworld(0, self.max_level, screen, self.create_level)
         self.status = 'overworld'
-        self.overworld_bg_music.play(loops = -1)
 
         # user interface
         self.ui = UI(screen)
 
-        
-
     def create_level(self, current_level):
         self.level = Level(current_level, screen, self.create_overworld, self.change_coins, self.change_health)
         self.status = 'level'
-        self.overworld_bg_music.stop()
-        self.level_bg_music.play(loops = -1)
     
     def create_overworld(self, current_level, new_max_level):
         if new_max_level > self.max_level:
             self.max_level = new_max_level
         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
         self.status = 'overworld'
-        self.overworld_bg_music.play(loops = -1)
-        self.level_bg_music.stop()
 
     def change_coins(self, amount):
         self.coins += amount
@@ -57,8 +44,6 @@ class Game:
             self.max_level = 0
             self.overworld = Overworld(0, self.max_level, screen, self.create_level)
             self.status = 'overworld'
-            self.level_bg_music.stop()
-            self.overworld_bg_music.play(loops = -1)
 
     def run(self):
         if self.status == 'overworld':
@@ -84,7 +69,6 @@ while True:
             pygame.quit()
             sys.exit()
     
-    screen.fill('grey')
     game.run()
 
     pygame.display.update()
